3abc9e4abd01e331ef24dcebd466cafe9297c300
Meghan King <mfking@uvm.edu>
2017-11-03 11:24:31 -0400

    Initial Commit

---
+++ Distribution-Data-Persistence-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Distribution-Data-Persistence-App
+//
+//  Created by Meghan King on 11/3/17.
+//  Copyright © 2017 Meghan King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Distribution-Data-Persistence-AppTests/Distribution_Data_Persistence_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Distribution_Data_Persistence_AppTests.swift
+//  Distribution-Data-Persistence-AppTests
+//
+//  Created by Meghan King on 11/3/17.
+//  Copyright © 2017 Meghan King. All rights reserved.
+//
+
+import XCTest
+@testable import Distribution_Data_Persistence_App
+
+class Distribution_Data_Persistence_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
